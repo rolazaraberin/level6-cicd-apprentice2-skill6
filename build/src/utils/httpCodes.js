8bc4fa5947bd79c1e613d60b76b0fc6550653360
Rolazar Aberin <rolazaraberin@gmail.com>
2023-11-29 00:39:45 -0800

    New build

---
+++ build/src/utils/httpCodes.js
@@ -0,0 +1,1 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const httpCodes={success:{created:201,general:200,ok:200},error:{badRequest:400,conflict:409,forbiddenUser:403,general:400,unauthorized:401,unauthenticated:401,incorrectPassword:401,incorrectCredentials:401,serverError:500},redirect:{general:300}};exports.default=httpCodes;
